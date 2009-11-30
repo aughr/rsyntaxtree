61d6cecaa7cdf4427c09dd96d821901df22a6b10
Andrew Bloomgarden <stalkingtiger@gmail.com>
2009-11-29 21:09:14 -0500

    Rewrote most of the main controller to use a PORO proxy.

--- app/controllers/rsyntaxtree_controller.rb
+++ app/controllers/rsyntaxtree_controller.rb
@@ -25,7 +25,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 require "rsyntaxtree"
-require 'cgi'
 
 class RsyntaxtreeController < ApplicationController
 
@@ -34,157 +33,46 @@ class RsyntaxtreeController < ApplicationController
   end
 
   def build_query
-
     if !params[:data] || params[:data].empty?
       render(:text => "There is no text to parse")
-    else
-      format     = params[:format]
-      symmetrize =params[:symmetrize]
-      color      = params[:color]
-      terminal   = params[:terminal]
-      autosub    = params[:autosub]
-      serif      = params[:serif]
-      fontsize   = params[:fontsize]
-      new_data1  = CGI::escapeHTML(params[:data])
-      new_data2  = new_data1.gsub(/'/, "-PRIME-")
-      text =<<EOD
-<div id='phrase'>#{new_data1}</div>
-<div id='graph'>
-<img src='/rsyntaxtree/draw_graph?data=#{new_data2}&format=#{format}&symmetrize=#{symmetrize}&color=#{color}&terminal=#{terminal}&autosub=#{autosub}&serif=#{serif}&fontsize=#{fontsize}' alt='#{new_data2}'/>
-<br /><br />
-<small>
-<a href='/rsyntaxtree/draw_graph?data=#{new_data2}&format=svg&symmetrize=#{symmetrize}&color=#{color}&terminal=#{terminal}&autosub=#{autosub}&serif=#{serif}&fontsize=#{fontsize}'>[Download SVG]</a>
-</small>
-</div>
-EOD
-      render(:text => text)
     end
-
   end
 
   def draw_graph
+    @tree = SyntaxTree.new params
+    image = @tree.render
+    disposition = @tree.svg? ? "attachment" : "inline"
+    filename = "syntaxtree.#{@tree.format}"
+    if image
+      send_data image, :disposition => disposition, :filename => filename
+    else
+      render :nothing => true, :status => :unprocessable_entity
+    end
+    GC.start
+  end
 
-      symmetrize = params[:symmetrize] == "on" ? true : false
-      color      = params[:color]      == "on" ? true : false
-      autosub    = params[:autosub]    == "on" ? true : false
-      with_serif = params[:serif]      == "on" ? true : false
-      fontsize   = params[:fontsize].to_i
-      data       = params[:data].gsub(/-PRIME-/, "'")
-      format     = params[:format].strip
-      terminal   = params[:terminal].strip
-      filename   = "syntree"
-
-      font_serif      = File.expand_path("#{RAILS_ROOT}/fonts/vera/VeraSe.ttf")
-      font_nonserif   = File.expand_path("#{RAILS_ROOT}/fonts/vera/Vera.ttf")
-      font_mbserif    = File.expand_path("#{RAILS_ROOT}/fonts/ipa/ipamp.ttf")
-      font_mbnonserif = File.expand_path("#{RAILS_ROOT}/fonts/ipa/ipagp.ttf")
-      font_mbnonja    = File.expand_path("#{RAILS_ROOT}/fonts/wqy-zenhei/wqy-zenhei.ttf")
-
-      if multibyte?(data)
-        if japanese?(data) || !File.exist?(font_mbnonja)
-          if with_serif   
-            font = font_mbserif
-          else
-            font = font_mbnonserif
-          end
-        else
-          font = font_mbnonja
-        end
-      else
-        if with_serif
-          font = font_serif
-        else
-          font = font_nonserif
+  def count_brackets
+    text = params['text'].strip
+    text_r = text.split(//)
+    open_br, close_br = [], []
+    text_r.each do |chr|
+      if chr == '['
+        open_br.push(chr)
+      elsif chr == ']'
+        close_br.push(chr)
+        if open_br.length < close_br.length
+          break
         end
       end
-
-      sp = StringParser.new(CGI.unescapeHTML(data))
-      if(!sp.validate)
-        e_message = "Error: problem in input sentence"
-        sp = StringParser.new(e_message)
-        sp.parse
-        elist =sp.get_elementlist
-        graph = TreeGraph.new(elist, true, false, false, font_nonserif, 14)
-        image_data = graph.to_blob
-        send_data( image_data,
-                   :disposition=> 'inline',
-                   :type => 'image/png',
-                   :filename => "error.png")
-      else
-        sp.parse
-        sp.auto_subscript if autosub
-        elist = sp.get_elementlist
-
-        case format
-        when /svg/
-          engine = SVGGraph
-        when /(gif|jpg|bmp)/
-          engine = TreeGraph
-        else
-          engine = TreeGraph
-          format = "png"
-        end
-        graph = engine.new(elist, symmetrize, color, terminal, font, fontsize)
-        if format == "svg"
-          send_data(graph.svg_data,
-                    :filename => 'synatxtree.svg')
-        else
-          image_data = graph.to_blob(format)
-          graph.destroy
-          send_data( image_data,
-                     :disposition=> "inline",
-                     :type => "image/#{format}",
-                     :filename => "syntaxtree.#{format}")
-          graph.destroy
-        end
-      end
-      GC.start
     end
 
-    def multibyte?(text)
-      result = false
-      text.strip.split(//).each do |chr|
-        unless /([!-~]|\s)/ =~ chr
-          result = true; break
-        end
-      end
-      return result
+    if open_br.length == close_br.length
+      message = "&nbsp;"
+    else
+      message = "Open and close brackets do not match up."
     end
 
-    def japanese?(text) #check if text contains hiragana or katakana
-      result = false
-      text.strip.split(//).each do |chr|
-        if /(\xe3(\x82[\xa1-\xbf]|\x83[\x80-\xbf]))/ =~ chr ||
-           /(\xe3(\x81[\x80-\xbf]|\x82[\x80-\xa0]))/ =~ chr
-           #|| /[一-龠]+/ =~ chr
-          result = true; break
-        end
-      end
-      return result
-    end
-
-    def count_brackets
-      text = params['text'].strip
-      text_r = text.split(//)
-      open_br, close_br = [], []
-      text_r.each do |chr|
-        if chr == '['
-          open_br.push(chr)
-        elsif chr == ']'
-          close_br.push(chr)
-          if open_br.length < close_br.length
-            break
-          end
-        end
-      end
-
-      if open_br.length == close_br.length
-        message = "&nbsp;"
-      else
-        message = "Open and close brackets do not match up."
-      end
-
-      render(:text => message)
-    end
+    render(:text => message)
+  end
 
 end
---
+++ app/models/syntax_tree.rb
@@ -0,0 +1,90 @@
+# just a PORO, not ActiveRecord
+class SyntaxTree
+  FONT_SERIF      = File.expand_path("#{RAILS_ROOT}/fonts/vera/VeraSe.ttf")
+  FONT_NONSERIF   = File.expand_path("#{RAILS_ROOT}/fonts/vera/Vera.ttf")
+  FONT_MBSERIF    = File.expand_path("#{RAILS_ROOT}/fonts/ipa/ipamp.ttf")
+  FONT_MBNONSERIF = File.expand_path("#{RAILS_ROOT}/fonts/ipa/ipagp.ttf")
+  FONT_MBNONJA    = File.expand_path("#{RAILS_ROOT}/fonts/wqy-zenhei/wqy-zenhei.ttf")
+  
+  def initialize(options={})
+    options[:format] ||= 'png'
+    options[:serif] = options[:serif] == 'on'
+    options[:fontsize] ||= 14
+    options[:fontsize] = options[:fontsize].to_i
+    options[:terminal] ||= "triangle"
+    
+    @data = options.delete(:data) || ""
+    @graph_engine = (options[:format] =~ /svg/ ? SVGGraph : TreeGraph)
+    @options = options
+  end
+  
+  def render
+    parser = StringParser.new(@data)
+    
+    if(!parser.validate)
+      nil
+    else
+      parser.parse
+      parser.auto_subscript if @options[:autosub]
+      elist = parser.get_elementlist
+      
+      graph = @graph_engine.new(elist, @options[:symmetrize], @options[:color], @options[:terminal], font, @options[:fontsize])
+      if @options[:format] == "svg"
+        image_data = graph.svg_data
+      else
+        image_data = graph.to_blob(@options[:format])
+      end
+      graph.destroy
+      image_data
+    end
+  end
+  
+  def svg?
+    true if @options[:format] =~ /svg/
+  end
+  
+  def format
+    @options[:format]
+  end
+  
+  protected
+  
+  def data_for_url
+  end
+  
+  def font
+    if multibyte?
+      if japanese? || !File.exist?(FONT_MBNONJA)
+        if @options[:serif]   
+          FONT_MBSERIF
+        else
+          FONT_MBNONSERIF
+        end
+      else
+        FONT_MBNONJA
+      end
+    else
+      if @options[:serif]
+        FONT_SERIF
+      else
+        FONT_NONSERIF
+      end
+    end
+  end
+  
+  def multibyte?
+    @data.strip.split(//).each do |chr|
+      return true unless /([!-~]|\s)/ =~ chr
+    end
+    false
+  end
+
+  def japanese? #check if text contains hiragana or katakana
+    @data.strip.split(//).each do |chr|
+      return true if /(\xe3(\x82[\xa1-\xbf]|\x83[\x80-\xbf]))/ =~ chr ||
+                     /(\xe3(\x81[\x80-\xbf]|\x82[\x80-\xa0]))/ =~ chr
+    end
+    false
+  end
+  
+end
